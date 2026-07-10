fd81666264f89d11b6545af15112966f671673cc
vitalii <pellesh011@gmail.com>
2026-07-10 15:15:07 +0300

    feat: implement PBAC authorization with Keycloak UMA

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller.js';
 import { KeycloakStrategy } from './strategies/keycloak.strategy.js';
+import { KeycloakAuthzService } from '../keycloak/keycloak-authz.service.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
-  imports: [
-    PrismaModule,
-    PassportModule.register({ defaultStrategy: 'keycloak' }),
-  ],
+  imports: [PrismaModule, HttpModule, PassportModule.register({ defaultStrategy: 'keycloak' })],
   controllers: [AuthController],
-  providers: [KeycloakStrategy],
+  providers: [KeycloakStrategy, KeycloakAuthzService],
+  exports: [KeycloakAuthzService],
 })
 export class AuthModule {}
